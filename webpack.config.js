71dbfcb1496038b0a4929ceafbcf6397fa7fe776
Kymani Stephens <kymanist00@gmail.com>
2023-04-21 22:08:51 -0700

    save

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'api-test-2',
       template: './src/index.html',
       inject: 'body'
     }),
